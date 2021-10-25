a83d02cde0a352bcaa4c496a0b09d62676d5ae38
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2021-10-25 13:16:35 -0600

    Added getter and setter for the name field

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -2,4 +2,12 @@ package org.paumard.model;
 
 public class User {
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
