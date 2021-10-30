e3e40c956a949b10e2c47abfb0c4f479ab34cd5d
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2021-10-30 11:27:21 -0600

    Added getter and setter for age

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -11,4 +11,12 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
